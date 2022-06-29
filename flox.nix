269f19f43942324af066e8689dadd4f30a856b4b
Yannik Sander <yannik@floxdev.com>
2022-06-29 17:51:42 +0200

    Migrate to new capacitor interface

--- flox.nix
+++ flox.nix
@@ -1,44 +1,51 @@
-{ inputs, lib, has, auto,... }:
+{ inputs, lib, has, auto, ... }:
 {
-  
+
   ############################################
   #        Company Root Configuration        #
   ############################################
 
-  __reflect.systems = ["aarch64-darwin" "aarch64-linux" "x86_64-darwin" "x86_64-linux"];
-  __reflect.stabilities = {
+  config = {
+    stabilities = {
       default = inputs.nixpkgs-stable;
       stable = inputs.nixpkgs-stable;
       unstable = inputs.nixpkgs-unstable;
-      staging =  inputs.nixpkgs-staging;
+      staging = inputs.nixpkgs-staging;
     };
 
-  __reflect.projects = { 
-      inherit (inputs.index) company; 
+    projects = {
+      inherit (inputs.index) company;
       inherit (inputs) floxpkgs;
-  };
+    };
 
-  __reflect.adopted =  { };
+    adopted = { };
+    
+    extraPlugins = [
+      (inputs.capacitor.plugins.allLocalResources {})
+      (inputs.capacitor.plugins.templates {})
+    ];
+ 
+  };
 
 
   ############################################
   #   Top-Level Re-Exports and Definitions   #
   ############################################
 
-  packages = has.both
-   # == { 
-   #  hello-upstream = {inputs',...}: inputs'.floxpkgs.legacyPackages.hello-upstream;
-   #}
-    (auto.reexport inputs.floxpkgs ["hello-upstream"])
-    {};
+  packages = 
+    (auto.reexport inputs.floxpkgs [ "hello-upstream" ])
+    # == { 
+    #  hello-upstream = {inputs',...}: inputs'.floxpkgs.legacyPackages.hello-upstream;
+    #}
+    // { };
 
-  apps = {};
+  apps = { };
 
-  devShells = {};
+  devShells = { };
 
   lib = {
-    testyou = {...}: lib.trace "you";  
-    testme = {...}: lib.testyou "hello";
+    testyou = { ... }: lib.trace "you";
+    testme = { ... }: lib.testyou "hello";
   };
 
   index = inputs.index;
